cc5b905e123ce87143b7b4b0a7126b05d9f177d2
beschofield <beschofield@users.noreply.github.com>
2017-09-20 12:59:58 -0500

    Create 3-template.cpp

---
+++ Initial-Assessment/3-template.cpp
@@ -0,0 +1,14 @@
+// headers go here
+
+int main ()
+{
+  // TODO: take in the user input for desired operation
+  // TODO: call the appropriate function
+}
+
+int addition(int a, int b)
+{
+  // TODO: fill this in with your own code and use it to add the other operations
+
+  return -1;
+}
