33347bb50388ee70c117561b1f0a7bbc1f688949
beschofield <beschofield@users.noreply.github.com>
2017-12-07 10:51:58 -0600

    Create 4-template.cpp

---
+++ Initial-Assessment/4-template.cpp
@@ -0,0 +1,14 @@
+// headers go here
+
+int main ()
+{
+  // TODO: take in the user input for desired operation
+  // TODO: call the appropriate function
+}
+
+int fibonacci(int n)
+{
+  // TODO: fill this in with your own code
+
+  return -1;
+}
